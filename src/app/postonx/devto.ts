8d4251f76f043d687eb0dbef4646f2f84a4dc28a
Abhinav <85792055+AbhinavTheDev@users.noreply.github.com>
2024-12-05 09:53:33 +0530

    minor

--- src/app/postonx/devto.ts
+++ src/app/postonx/devto.ts
@@ -1,10 +1,14 @@
 export const postonDevTo = async (title: string, markdown: string, url: string) => {
     try {
+        const apiKey = process.env.DevTo_API_Key;
+        if (!apiKey) {
+            throw new Error('DevTo API key is not defined');
+        }
         const response = await fetch('https://dev.to/api/articles', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
-              'api-key': process.env.DevTo_API_Key,
+              'api-key': apiKey,
             },
             body: JSON.stringify({
               article: {
